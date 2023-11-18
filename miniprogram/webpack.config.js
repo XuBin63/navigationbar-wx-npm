52c5f6955e2f606a6d8ae5fa38117d036eddcca5
Brandon <xubin63@163.com>
2023-11-18 15:34:22 +0800

    默认

--- miniprogram/webpack.config.js
+++ miniprogram/webpack.config.js
@@ -7,7 +7,7 @@ module.exports = {
 	},
 	output: {
 		// filename: 'index.js',
-		path: path.resolve(__dirname, '../dist'),
+		path: path.resolve(__dirname, 'dist'),
 		// clean: true,
 	},
 	module: {
