5d48fcb658560060c9afe3d75776c8bb4c858367
Xu,Bin <xubin63@163.com>
2024-05-10 14:20:30 +0800

    1.0.8取消压缩

--- miniprogram/webpack.config.js
+++ miniprogram/webpack.config.js
@@ -3,7 +3,6 @@ module.exports = {
 	// entry: './components/navigationbar-wx/index.js',
 	entry: {
 		// index: './components/navigationbar-wx/index.js',
-		config: './components/navigationbar-wx/config.js',
 	},
 	output: {
 		// filename: 'index.js',
